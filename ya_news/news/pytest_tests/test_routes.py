525f700584a4e0953767cf4b04166859c0790d24
Стас Баринов <stanislavbarinov0@gmail.com>
2024-04-08 16:29:35 +0300

    test4

--- ya_news/news/pytest_tests/test_routes.py
+++ ya_news/news/pytest_tests/test_routes.py
@@ -7,26 +7,39 @@ pytestmark = pytest.mark.django_db
 
 
 @pytest.mark.parametrize(
-    'client_fixture, expected_status',
-    (
-        ('client', HTTPStatus.OK),
-        ('admin_client', HTTPStatus.NOT_FOUND),
-        ('author_client', HTTPStatus.OK)
-    )
-)
-@pytest.mark.parametrize(
-    'url_fixture',
-    (
-        'url_home',
-        'url_login',
-        'url_logout',
-        'url_signup',
-        'url_detail'
-    )
+    'client_fixture, url_fixture, expected_status',
+    [
+        (
+            'client',
+            'url_home',
+            HTTPStatus.OK
+        ),
+        (
+            'admin_client',
+            'url_edit',
+            HTTPStatus.NOT_FOUND
+        ),
+        (
+            'admin_client',
+            'url_delete',
+            HTTPStatus.NOT_FOUND
+        ),
+        (
+            'author_client',
+            'url_edit',
+            HTTPStatus.OK
+        ),
+        (
+            'author_client',
+            'url_delete',
+            HTTPStatus.OK
+        )
+    ]
 )
 def test_page_availability(
-    client_fixture, expected_status, url_fixture, request
+    client_fixture, url_fixture, expected_status, request
 ):
+    """Проверка статусов всех страниц для разных клиентов."""
     client = request.getfixturevalue(client_fixture)
     url = request.getfixturevalue(url_fixture)
     response = client.get(url)
@@ -41,6 +54,7 @@ def test_page_availability(
     )
 )
 def test_redirects(client_fixture, url_fixture, url_login, request):
+    """Проверка редиректов."""
     client = request.getfixturevalue(client_fixture)
     url = request.getfixturevalue(url_fixture)
     expected_url = f'{url_login}?next={url}'
